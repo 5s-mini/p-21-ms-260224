5ff1a10b28c898fd7dbd81547097f86339aa720b
5s-mini <dnclsehd123@naver.com>
2026-03-07 02:05:50 +0900

    refactor: QuestionController에 AnswerForm 상속

--- src/main/java/com/back/question/QuestionController.java
+++ src/main/java/com/back/question/QuestionController.java
@@ -1,5 +1,7 @@
 package com.back.question;
 
+import com.back.answer.AnswerForm;
+import jakarta.validation.Valid;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
@@ -25,7 +27,7 @@ public class QuestionController {
     }
 
     @GetMapping(value = "/detail/{id}")
-    public String detail(Model model, @PathVariable("id") Integer id) {
+    public String detail(Model model, @PathVariable("id") Integer id, AnswerForm answerForm) {
         Question question = this.questionService.getQuestion(id);
         model.addAttribute("question", question);
         return "question_detail";
