176e2e3b2a3e5624a811297bc2c69125c9f32de2
5s-mini <dnclsehd123@naver.com>
2026-03-06 17:42:55 +0900

    feat: MainController 추가

--- src/main/java/com/back/P21Ms260224Application.java
+++ src/main/java/com/back/P21Ms260224Application.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class P21Ms260224Application {
-
     public static void main(String[] args) {
         SpringApplication.run(P21Ms260224Application.class, args);
     }
-
 }
